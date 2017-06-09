2c1c717d0435208c4bf285986a4bcdb8db6fe511
Ashley Durbin <ashleydurbin@Ashleys-MacBook-Air.local>
2017-06-09 15:50:24 -0700

    Prob 2 compleat

--- Test.js
+++ Test.js
@@ -7,14 +7,14 @@ function testSum() { //eslint-disable-line
     console.log('%c TEST FOR sum() FAILS', 'color: red');
   }
 }
-//
-// function testMultiply() { //eslint-disable-line
-//   if (multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
-//     console.log('%c TEST FOR multiply() PASSES', 'color: green');
-//   } else {
-//     console.log('%c TEST FOR multiply() FAILS', 'color: red');
-//   }
-// }
+
+function testMultiply() { //eslint-disable-line
+  if (multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
+    console.log('%c TEST FOR multiply() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR multiply() FAILS', 'color: red');
+  }
+}
 //
 // function testSumAndMultiply() { //eslint-disable-line
 //   if (sumAndMultiply(4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.') {
