49b087022b0c7b35713f74a325615f42570aba27
Ashley Durbin <ashleydurbin@Ashleys-MacBook-Air.local>
2017-06-09 14:50:37 -0700

    prob1 complete

--- Test.js
+++ Test.js
@@ -7,22 +7,22 @@ function testSum() { //eslint-disable-line
     console.log('%c TEST FOR sum() FAILS', 'color: red');
   }
 }
-
-function testMultiply() { //eslint-disable-line
-  if (multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
-    console.log('%c TEST FOR multiply() PASSES', 'color: green');
-  } else {
-    console.log('%c TEST FOR multiply() FAILS', 'color: red');
-  }
-}
-
-function testSumAndMultiply() { //eslint-disable-line
-  if (sumAndMultiply(4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.') {
-    console.log('%c TEST FOR sumAndMultiply() PASSES', 'color: green');
-  } else {
-    console.log('%c TEST FOR sumAndMultiply() FAILS', 'color: red');
-  }
-}
+//
+// function testMultiply() { //eslint-disable-line
+//   if (multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
+//     console.log('%c TEST FOR multiply() PASSES', 'color: green');
+//   } else {
+//     console.log('%c TEST FOR multiply() FAILS', 'color: red');
+//   }
+// }
+//
+// function testSumAndMultiply() { //eslint-disable-line
+//   if (sumAndMultiply(4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.') {
+//     console.log('%c TEST FOR sumAndMultiply() PASSES', 'color: green');
+//   } else {
+//     console.log('%c TEST FOR sumAndMultiply() FAILS', 'color: red');
+//   }
+// }
 
 function testSumArray() { //eslint-disable-line
   if (sumArray(testArray)[1] === '2,3,4 was passed in as an array of numbers, and 9 is their sum.') {
