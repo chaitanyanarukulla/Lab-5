f1e60a47be327173df5a51fc97bc99a97e68f669
Ashley Durbin <ashleydurbin@Ashleys-MacBook-Air.local>
2017-06-09 17:13:09 -0700

    Prob3 Done

--- Aapp.js
+++ Aapp.js
@@ -27,16 +27,16 @@ Write a function called multiply() that takes in two numbers as arguments and re
 "The product of 5 and 9 is 45."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// TODO: Write your code here
+// // TODO: Write your code here
 function multiply(a,b){ //eslint-disable-line
   var product = a * b;
   var message = 'The product of ' + a + ' and ' + b + ' is ' + product + '.';
   console.log(message);
   return [product, message];
 }
-
-// TODO: Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+//
+// // TODO: Here is the test for multiply(); uncomment it to run it
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,11 +50,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // TODO: Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var add = sum(a,b)[0];
+  var add2 = sum(add, c)[0];
+  var mult = multiply(a, b)[0];
+  var mult2 = multiply(mult, c)[0];
+  var mess1 = a + ' and ' + b + ' and ' + c + ' sum to ' + add2 + '.';
+  var mess2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + mult2 + '.';
+  return [add2, mult2, mess1, mess2];
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
